c2d92e691ea2497d7fb422f2853da23c4737b0be
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2026-04-18 15:26:08 +0200

    add one to one inverse

--- SetTheory/Functions/one_to_one_inverse.lean
+++ SetTheory/Functions/one_to_one_inverse.lean
@@ -13,7 +13,12 @@ theorem one_to_one_inverse (F: Set) (h0: F is one to one)
       have h7 := h0.right.right y x z ⟨h5,h6⟩
       exact h7 ▸ h7
     exact ⟨(inverse_is_relation F h1), h2⟩
-  have h2: F[h1]⁻¹ is single rooted := by
-
-    sorry
-  sorry
+  have sr: F[h1]⁻¹ is single rooted := by
+    have h3: ∀x y z: Set, (x,y)∈F[h1]⁻¹ ∧ (z,y)∈F[h1]⁻¹ → z=x := by
+      intro x y z ⟨h4,h5⟩
+      have h6: (y,x)∈F := (inverse_xy F x y h1).mp h4
+      have h7: (y,z)∈F := (inverse_xy F z y h1).mp h5
+      have h8 := (h0.left.right y x z ⟨h6,h7⟩)
+      exact h8▸h8
+    exact ⟨(inverse_is_relation F h1), h3⟩
+  exact ⟨sv,sr⟩
